c6a925cc79aca1a6da82bf21329304c28f6d3ef4
Raihan454 <raihanmarsa712@gmail.com>
2022-11-07 17:02:06 +0700

    add a node list

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace single_linked_list
+{
+    //each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null();
+        }
+        public void addNote() //add a node in the List
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll number of the student");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if((START == null) || (rollNo <= START.rollNumber))
+            {
+                if((START ! = null) &&(rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newmode.next = START;
+                START = newnode;
+                return;
+            }
+        }
+
+    }
+    class Program
+    { }
+}
